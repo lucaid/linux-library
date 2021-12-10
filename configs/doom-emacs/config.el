747fafab62ac2f8d40fd81b98f7d380e74fd5e80
lucaid <lucaid@protonmail.com>
2021-12-10 00:11:02 -0800

    adjust header font

--- configs/doom-emacs/config.el
+++ configs/doom-emacs/config.el
@@ -124,7 +124,7 @@
 
 (custom-set-faces
   '(org-level-1 ((t (:inherit outline-1 :height 1.2))))
-  '(org-level-2 ((t (:inherit outline-2 :height 1.0))))
+  '(org-level-2 ((t (:inherit outline-2 :height 1.1))))
   '(org-level-3 ((t (:inherit outline-3 :height 1.0))))
   '(org-level-4 ((t (:inherit outline-4 :height 1.0))))
   '(org-level-5 ((t (:inherit outline-5 :height 1.0)))))
