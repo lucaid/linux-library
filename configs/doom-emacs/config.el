5d612666d8bae548dc82b9e872852cd93738a0d5
lucaid <lucaid@protonmail.com>
2021-12-10 21:21:55 -0800

    fixed org-agenda directories

--- configs/doom-emacs/config.el
+++ configs/doom-emacs/config.el
@@ -31,7 +31,7 @@
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
 (setq org-directory "~/Documents/org")
-(setq org-agenda-files (directory-files-recursively "~/Documents/org/" "\.org$"))
+(setq org-agenda-files (directory-files-recursively "~/Documents/org/" "\.org$" t))
 ;; Here are some additional functions/macros that could help you configure Doom:
 ;;
 ;; - `load!' for loading external *.el files relative to this one
